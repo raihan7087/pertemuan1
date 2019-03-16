a4388d08e09bf9662e8ec42e21c7585f4c2bc104
raihan7087 <raihan.shubhiy@students.amikom.ac.id>
2019-03-17 00:15:23 +0700

    kalkulator project

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace CalculatorConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Aplikasi Calculator";
+
+            Console.Write("Inputan nilai a = ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Inputan nilai b = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Hasil penambahan " + a + " + " + b + " = " + Penambahan(a, b));
+            Console.WriteLine("Hasil pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
+            Console.WriteLine("Hasil perkalian {0} * {1} = {2}", a, b, Perkalian(a, b));
+            Console.WriteLine("Hasil pembagian {0} / {1} = {2}", a, b, Pembagian(a, b));
+
+            Console.WriteLine("\nTekan Sembarang key untuk keluar");
+            Console.ReadKey();
+
+
+        }
+
+
+        static int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+
+        static int Pembagian(int a, int b)
+        {
+            return a / b;
+        }
+
+    }
+
+
+
+
+}
